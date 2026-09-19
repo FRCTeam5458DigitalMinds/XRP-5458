dbf1dd9736e71b1243bf8b53e469c5e1ebb5a12a
deanbunn <deanbunn@gmail.com>
2026-09-18 17:43:51 -0700

    20260918_1743hrs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,62 +4,96 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-
-import static frc.robot.Constants.DriveConstants.*;
-
-import frc.robot.commands.DriveDistanceCommand;
-
-import frc.robot.commands.TurnCommand;
+import edu.wpi.first.wpilibj.GenericHID;
+//import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+//import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.AutonomousDistance;
+import frc.robot.commands.AutonomousTime;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.xrp.XRPOnBoardIO;
+import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+//import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
 public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final XRPOnBoardIO m_onboardIO = new XRPOnBoardIO();
+  private final Arm m_arm = new Arm();
 
-    private final Drivetrain drivetrain = new Drivetrain();
+  // // Assumes a gamepad plugged into channel 0
+  // private final Joystick m_controller = new Joystick(0);
 
-    public RobotContainer() {
-        // No controller or joystick required.
-    }
+  // Create SmartDashboard chooser for autonomous routines
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-    public Command getAutonomousCommand() {
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
 
-        return Commands.sequence(
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // // Default command is arcade drive. This will run unless another command
+    // // is scheduled over it.
+    // m_drivetrain.setDefaultCommand(getArcadeDriveCommand());
 
-            // Reset sensors before beginning.
-            Commands.runOnce(
-                drivetrain::resetSensors,
-                drivetrain
-            ),
+    // Example of how to use the onboard IO
+    Trigger userButton = new Trigger(m_onboardIO::getUserButtonPressed);
+    userButton
+        .onTrue(new PrintCommand("USER Button Pressed"))
+        .onFalse(new PrintCommand("USER Button Released"));
 
-            // Drive forward 1 meter.
-            new DriveDistanceCommand(
-                drivetrain,
-                DISTANCE_1_METERS
-            ),
+    // JoystickButton joystickAButton = new JoystickButton(m_controller, 1);
+    // joystickAButton
+    //     .onTrue(new InstantCommand(() -> m_arm.setAngle(45.0), m_arm))
+    //     .onFalse(new InstantCommand(() -> m_arm.setAngle(0.0), m_arm));
 
-            // Turn 90 degrees.
-            new TurnCommand(
-                drivetrain,
-                TURN_ANGLE_DEGREES
-            ),
+    // JoystickButton joystickBButton = new JoystickButton(m_controller, 2);
+    // joystickBButton
+    //     .onTrue(new InstantCommand(() -> m_arm.setAngle(90.0), m_arm))
+    //     .onFalse(new InstantCommand(() -> m_arm.setAngle(0.0), m_arm));
 
-            // Drive forward another 1 meter.
-            new DriveDistanceCommand(
-                drivetrain,
-                DISTANCE_2_METERS
-            ),
+    // Setup SmartDashboard options
+    m_chooser.setDefaultOption("Auto Routine Distance", new AutonomousDistance(m_drivetrain));
+    m_chooser.addOption("Auto Routine Time", new AutonomousTime(m_drivetrain));
+    SmartDashboard.putData(m_chooser);
+  }
 
-            // Stop.
-            Commands.runOnce(
-                drivetrain::stop,
-                drivetrain
-            )
-        );
-    }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return m_chooser.getSelected();
+  }
 
-    private static final double DISTANCE_1_METERS = Constants.DriveConstants.DISTANCE_1_METERS;
-
-    private static final double DISTANCE_2_METERS = Constants.DriveConstants.DISTANCE_2_METERS;
+  /**
+   * Use this to pass the teleop command to the main {@link Robot} class.
+   *
+   * @return the command to run in teleop
+   */
+  // public Command getArcadeDriveCommand() {
+  //   return new ArcadeDrive(
+  //       m_drivetrain, () -> -m_controller.getRawAxis(1), () -> -m_controller.getRawAxis(2));
+  // }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,85 +1,150 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.xrp.XRPGyro;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
-
-import static frc.robot.Constants.DriveConstants.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
+  private static final double kGearRatio =
+      (30.0 / 14.0) * (28.0 / 16.0) * (36.0 / 9.0) * (26.0 / 8.0); // 48.75:1
+  private static final double kCountsPerMotorShaftRev = 12.0;
+  private static final double kCountsPerRevolution = kCountsPerMotorShaftRev * kGearRatio; // 585.0
+  private static final double kWheelDiameterInch = 2.3622; // 60 mm
 
-    private final XRPMotor leftMotor = new XRPMotor(LEFT_MOTOR);
-    private final XRPMotor rightMotor = new XRPMotor(RIGHT_MOTOR);
+  // The XRP has the left and right motors set to
+  // channels 0 and 1 respectively
+  private final XRPMotor m_leftMotor = new XRPMotor(0);
+  private final XRPMotor m_rightMotor = new XRPMotor(1);
 
-    private final Encoder leftEncoder =
-            new Encoder(LEFT_ENCODER_A, LEFT_ENCODER_B);
+  // The XRP has onboard encoders that are hardcoded
+  // to use DIO pins 4/5 and 6/7 for the left and right
+  private final Encoder m_leftEncoder = new Encoder(4, 5);
+  private final Encoder m_rightEncoder = new Encoder(6, 7);
 
-    private final Encoder rightEncoder =
-            new Encoder(RIGHT_ENCODER_A, RIGHT_ENCODER_B);
+  // Set up the differential drive controller
+  private final DifferentialDrive m_diffDrive =
+      new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
 
-    private final XRPGyro gyro = new XRPGyro();
+  // Set up the XRPGyro
+  private final XRPGyro m_gyro = new XRPGyro();
 
-    public Drivetrain() {
+  // Set up the BuiltInAccelerometer
+  private final BuiltInAccelerometer m_accelerometer = new BuiltInAccelerometer();
 
-        // XRP's right motor runs in the opposite physical direction.
-        rightMotor.setInverted(true);
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    SendableRegistry.addChild(m_diffDrive, m_leftMotor);
+    SendableRegistry.addChild(m_diffDrive, m_rightMotor);
 
-        // Configure encoder distance in meters.
-        double distancePerPulse =
-                (Math.PI * WHEEL_DIAMETER_METERS)
-                / ENCODER_COUNTS_PER_REVOLUTION;
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightMotor.setInverted(true);
 
-        leftEncoder.setDistancePerPulse(distancePerPulse);
-        rightEncoder.setDistancePerPulse(distancePerPulse);
+    // Use inches as unit for encoder distances
+    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    resetEncoders();
+  }
 
-        resetEncoders();
-        resetGyro();
-    }
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+  }
 
-    public void setMotors(double leftSpeed, double rightSpeed) {
-        leftMotor.set(leftSpeed);
-        rightMotor.set(rightSpeed);
-    }
+  public void resetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
 
-    public void stop() {
-        leftMotor.stopMotor();
-        rightMotor.stopMotor();
-    }
+  public int getLeftEncoderCount() {
+    return m_leftEncoder.get();
+  }
 
-    public double getLeftDistanceMeters() {
-        return leftEncoder.getDistance();
-    }
+  public int getRightEncoderCount() {
+    return m_rightEncoder.get();
+  }
 
-    public double getRightDistanceMeters() {
-        return rightEncoder.getDistance();
-    }
+  public double getLeftDistanceInch() {
+    return m_leftEncoder.getDistance();
+  }
 
-    public double getAverageDistanceMeters() {
-        return (Math.abs(getLeftDistanceMeters())
-                + Math.abs(getRightDistanceMeters())) / 2.0;
-    }
+  public double getRightDistanceInch() {
+    return m_rightEncoder.getDistance();
+  }
 
-    public double getGyroAngle() {
-        return gyro.getAngle();
-    }
+  public double getAverageDistanceInch() {
+    return (getLeftDistanceInch() + getRightDistanceInch()) / 2.0;
+  }
 
-    public void resetEncoders() {
-        leftEncoder.reset();
-        rightEncoder.reset();
-    }
+  /**
+   * The acceleration in the X-axis.
+   *
+   * @return The acceleration of the XRP along the X-axis in Gs
+   */
+  public double getAccelX() {
+    return m_accelerometer.getX();
+  }
 
-    public void resetGyro() {
-        gyro.reset();
-    }
+  /**
+   * The acceleration in the Y-axis.
+   *
+   * @return The acceleration of the XRP along the Y-axis in Gs
+   */
+  public double getAccelY() {
+    return m_accelerometer.getY();
+  }
 
-    public void resetSensors() {
-        resetEncoders();
-        resetGyro();
-    }
+  /**
+   * The acceleration in the Z-axis.
+   *
+   * @return The acceleration of the XRP along the Z-axis in Gs
+   */
+  public double getAccelZ() {
+    return m_accelerometer.getZ();
+  }
 
-    @Override
-    public void periodic() {
-        // Optional telemetry for debugging.
-    }
+  /**
+   * Current angle of the XRP around the X-axis.
+   *
+   * @return The current angle of the XRP in degrees
+   */
+  public double getGyroAngleX() {
+    return m_gyro.getAngleX();
+  }
+
+  /**
+   * Current angle of the XRP around the Y-axis.
+   *
+   * @return The current angle of the XRP in degrees
+   */
+  public double getGyroAngleY() {
+    return m_gyro.getAngleY();
+  }
+
+  /**
+   * Current angle of the XRP around the Z-axis.
+   *
+   * @return The current angle of the XRP in degrees
+   */
+  public double getGyroAngleZ() {
+    return m_gyro.getAngleZ();
+  }
+
+  /** Reset the gyro. */
+  public void resetGyro() {
+    m_gyro.reset();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
